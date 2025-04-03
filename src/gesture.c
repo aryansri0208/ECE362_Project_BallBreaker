38ba8476cf06b0277cde4e6838cfa8dbed38f161
roshpat <pate1902@purdue.edu>
2025-04-03 12:59:03 -0400

    fixed error

--- src/gesture.c
+++ src/gesture.c
@@ -3,7 +3,17 @@
 
 void init_gesture_sensor() {
     // I2C and APDS-9960 configuration code for fast mode here
-    // Configure GPIO pins and I2C1 peripheral
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+
+    GPIOB->MODER &= ~(GPIO_MODER_MODER6 | GPIO_MODER_MODER7);
+    GPIOB->MODER |= GPIO_MODER_MODER6_1 | GPIO_MODER_MODER7_1;
+    GPIOB->AFR[0] |= (1 << (4 * 6)) | (1 << (4 * 7));
+
+    I2C1->TIMINGR = 0x00300F38;
+
+
+
 }
 
 Direction get_gesture_direction() {
