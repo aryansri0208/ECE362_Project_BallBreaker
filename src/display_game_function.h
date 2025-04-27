900cd7cce0775bc337602af55c56db654161d431
aishuuanand <113258081+aishuuanand@users.noreply.github.com>
2025-04-27 14:47:00 -0400

    Update display_game_function.h

--- src/display_game_function.h
+++ src/display_game_function.h
@@ -1,14 +1,6 @@
 #ifndef DISPLAY_GAME_FUNCTION_H
 #define DISPLAY_GAME_FUNCTION_H
 
-void draw_score();
-void draw_background();
-void draw_paddle();
-void draw_ball();
-void draw_bricks();
-void init_game_state();
-void move_ball_and_check_collision();
-void draw_game_frame();
-void display_game_over();
+void setup_game_screen();
 
 #endif
